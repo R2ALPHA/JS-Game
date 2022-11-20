6983fdb599f20b0105e3ea460f1455198e7cbb04
gourav anand <gourav.07anand@gmail.com>
2022-11-20 22:06:11 +0530

    refactor player class; fix minor issue

--- power-game/player.js
+++ power-game/player.js
@@ -4,6 +4,11 @@ import { FloatingMessage } from "./floatingMessages.js";
 
 export default class Player {
 
+    /**
+     * Constructor 
+     * 
+     * @param {Game} game is the game object  
+     */
     constructor(game) {
 
         this.game = game;
@@ -36,8 +41,10 @@ export default class Player {
     }
 
     /**
+     * Update player coordinate
      * 
      * @param {InputHandler} input is the input handler for the game 
+     * @param {number} deltaTime is the time diff between previous and current frame 
      */
     update(input, deltaTime) {
 
@@ -46,13 +53,7 @@ export default class Player {
 
         // Horizontal Movement
         this.x += this.speed;
-        if (input.isContainsKey(input.keyTypes.right && !this.currentState instanceof Hit)) {
-            this.speed = this.maxSpeed;
-        } else if (input.isContainsKey(input.keyTypes.left)) {
-            this.speed = -this.maxSpeed;
-        } else {
-            this.speed = 0;
-        }
+        this.setSpeed(input);
 
         // Horizontal boundaries
         this.x = this.x < 0 ? 0 : (this.x > this.game.width - this.width) ? this.game.width - this.width : this.x;
@@ -60,14 +61,47 @@ export default class Player {
         // Vertical movement
         this.y += this.vy;
 
+        // Vertical boundaries
+        this.y = this.y > this.game.height - this.height - this.game.groundMargin ? this.game.height - this.height - this.game.groundMargin : this.y;
+
+        this.handleSprites(deltaTime);
+    }
+
+    /**
+     * Set player's speed based on type of input 
+     * 
+     * @param {InputHandler} input is the input handler object 
+     */
+    setSpeed(input) {
+
+        if (input.isContainsKey(input.keyTypes.right) && !(this.currentState instanceof Hit)) {
+            this.speed = this.maxSpeed;
+        } else if (input.isContainsKey(input.keyTypes.left)) {
+            this.speed = -this.maxSpeed;
+        } else {
+            this.speed = 0;
+        }
+    }
+
+    /**
+     * Set player's vertical speed 
+     */
+    setVerticalSpeed() {
+
         if (!this.onGround()) {
             this.vy += this.weight;
         } else {
             this.vy = 0;
         }
+    }
 
-        // Vertical boundaries
-        this.y = this.y > this.game.height - this.height - this.game.groundMargin ? this.game.height - this.height - this.game.groundMargin : this.y;
+    /**
+     * Handle sprites 
+     * 
+     * @param {number} deltaTime is time diff between current and previous frame 
+     */
+    handleSprites(deltaTime) {
+
         if (this.frameTimer > this.frameInterval) {
             // Sprite animation
             this.frameX = this.frameX < this.maxFrame ? this.frameX + 1 : 0;
@@ -78,23 +112,32 @@ export default class Player {
     }
 
     /**
-     * 
      * Draw player object to the canvas 
      * 
      * @param {CanvasRenderingContext2D} context is canvas context
      */
     draw(context) {
 
+        // When in debug mode, draw player's boundary 
         if (this.game.debug) {
             context.strokeRect(this.x, this.y, this.width, this.height);
         }
         context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height);
     }
 
+    /**
+     * Returns whether player is on ground or not 
+     */
     onGround() {
         return this.y >= this.game.height - this.height - this.game.groundMargin;
     }
 
+    /**
+     * Set player's current state 
+     * 
+     * @param {State} state is state of player
+     * @param {number} speed is the game speed 
+     */
     setState(state, speed) {
 
         this.currentState = this.states[state];
@@ -102,19 +145,17 @@ export default class Player {
         this.currentState.enter();
     }
 
+    /**
+     * Check collision 
+     */
     checkCollision() {
         this.game.enemies.forEach(enemy => {
-            if (
-                enemy.x < this.x + this.width &&
-                enemy.x + enemy.width > this.x &&
-                enemy.y < this.y + this.height &&
-                enemy.y + enemy.height > this.y
-            ) {
+            if (this.isRectangleIntersect(enemy.x, enemy.y, enemy.width, enemy.height, this.x, this.y, this.width, this.height)) {
                 enemy.markedForDeletion = true;
-                this.game.collisions.push(new CollisionAnimation(this.game, enemy.x + enemy.width * 0.5, enemy.y + enemy.height * 0.5));
                 if (this.currentState instanceof Rolling || this.currentState instanceof Diving) {
                     this.game.score++;
                     this.game.floatingMessages.push(new FloatingMessage('+1', enemy.x, enemy.y, 150, 100));
+                    this.game.collisions.push(new CollisionAnimation(this.game, enemy.x + enemy.width * 0.5, enemy.y + enemy.height * 0.5));
                 } else {
                     this.setState(states.HIT, 0);
                     this.game.lives--;
@@ -124,5 +165,12 @@ export default class Player {
                 }
             }
         });
+    }
+
+    /**
+     * Returns whether two rectangle intersects or not 
+     */
+    isRectangleIntersect(x, y, width, height, x1, y1, width1, height1) {
+        return x < x1 + width1 && x + width > x1 && y < y1 + height1 && y + height > y1;
     }
 }
