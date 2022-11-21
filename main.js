36f511481d8316b260f42776a30c50ddc39d6d85
gourav anand <gourav.07anand@gmail.com>
2022-11-21 22:47:59 +0530

    add particles and typewriter to entry page

--- main.js
+++ main.js
@@ -6,12 +6,47 @@ var mainModule = (function () {
 
     // DOM Caching 
     const gameModeElement = document.getElementById('gameModule');
+    const typeWriterElement = document.getElementById('typeWriter');
 
     /**
      * Entry function for the module
      */
     function initialize() {
+
+        addParticles();
         addEventListener();
+        addTypeWriter();
+    }
+
+    /**
+     * Add particles through particle.js library
+     */
+    function addParticles() {
+        particlesJS.load('particles-js', './config.json', function () {
+            console.log('callback - particles.js config loaded');
+        });
+    }
+
+    /**
+     * Add type writer through typewriter.js library
+     */
+    function addTypeWriter() {
+
+        const typewriter = new Typewriter(typeWriterElement, {
+            loop: true,
+            cursorClassName: 'cursorSize' // SET TO MY CUSTOM CLASS NAME
+        });
+
+        typewriter.typeString('Hey, Thanks for jumping in')
+            .pauseFor(2500)
+            .deleteAll()
+            .typeString('My name is Gourav Ananad and I am a software developer based in INDIA.')
+            .pauseFor(2500)
+            .deleteAll()
+            .typeString('You can reach me at gourav.07anand[at]gmail.com')
+            .pauseFor(2500)
+            .start();
+
     }
 
     /**
